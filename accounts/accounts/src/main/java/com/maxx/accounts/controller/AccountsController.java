7ac40198b1f7cb61a7bb60fd079db17774542cbc
mahendra.chawla <mahendra.chawla26@gmail.com>
2024-01-28 17:39:33 +0530

    Schema and Error Response openAPI changes.

--- accounts/accounts/src/main/java/com/maxx/accounts/controller/AccountsController.java
+++ accounts/accounts/src/main/java/com/maxx/accounts/controller/AccountsController.java
@@ -2,10 +2,14 @@ package com.maxx.accounts.controller;
 
 import com.maxx.accounts.constants.AccountsConstants;
 import com.maxx.accounts.dto.CustomerDto;
+import com.maxx.accounts.dto.ErrorResponseDto;
 import com.maxx.accounts.dto.ResponseDto;
 import com.maxx.accounts.service.IAccountsService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
@@ -45,6 +49,22 @@ public class AccountsController {
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Fetch Account Details REST API",
+            description = "REST API to fetch Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses(
+            {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "HTTP Status OK"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "HTTP Status Internal Server Error"
+                    )
+            }
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
                                                                @Pattern(regexp = "(^$|[0-9]{10})",
@@ -55,6 +75,28 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
+    @Operation(
+            summary = "Update Account Details REST API",
+            description = "REST API to update Customer &  Account details based on a account number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@Valid@RequestBody CustomerDto customerDto){
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
@@ -69,6 +111,25 @@ public class AccountsController {
         }
     }
 
+    @Operation(
+            summary = "Delete Account & Customer Details REST API",
+            description = "REST API to delete Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
             @Pattern(regexp = "(^$|[0-9]{10})",
--- accounts/accounts/src/main/java/com/maxx/accounts/dto/ErrorResponseDto.java
+++ accounts/accounts/src/main/java/com/maxx/accounts/dto/ErrorResponseDto.java
@@ -1,5 +1,6 @@
 package com.maxx.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
@@ -7,14 +8,30 @@ import org.springframework.http.HttpStatus;
 import java.time.LocalDateTime;
 
 @Data @AllArgsConstructor
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold Error Response Information"
+)
 public class ErrorResponseDto {
 
+    @Schema(
+            description = "API Path invoked by the client"
+    )
     private String apiPath;
 
+    @Schema(
+            description = "Error code representing the error happened"
+    )
     private HttpStatus errorCode;
 
+    @Schema(
+            description = "Error message representing the error happened"
+    )
     private String errorMessage;
 
+    @Schema(
+            description = "Time representing when the error happened"
+    )
     private LocalDateTime errorTime;
 
 }
