b9edfd8dabe9a1bf1fa89c2df3a016c0c96f94ae
mahendra.chawla <mahendra.chawla26@gmail.com>
2024-04-23 16:36:29 +0530

    configure and enable actuator in Account,Loans and Cards MS

--- accounts/accounts/src/main/java/com/maxx/accounts/dto/AccountsContactInfoDto.java
+++ accounts/accounts/src/main/java/com/maxx/accounts/dto/AccountsContactInfoDto.java
@@ -1,5 +1,7 @@
 package com.maxx.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,7 +9,11 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String,String> contactDetails,
-                                     List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDto {
 
+    private String message;
+    private Map<String,String> contactDetails;
+    private List<String> onCallSupport;
 }
