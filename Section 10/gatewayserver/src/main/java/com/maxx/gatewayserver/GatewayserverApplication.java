fdfb0ec288c9dd676d57d2fdf8f4d31d123d2a45
mahendra.chawla <mahendra.chawla26@gmail.com>
2024-09-18 13:38:04 +0530

    Implementing retry pattern in loans service.

--- Section 10/gatewayserver/src/main/java/com/maxx/gatewayserver/GatewayserverApplication.java
+++ Section 10/gatewayserver/src/main/java/com/maxx/gatewayserver/GatewayserverApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -27,7 +29,10 @@ public class GatewayserverApplication {
 					.route(p -> p
 							.path("/maxxbank/loans/**")
 							.filters( f -> f.rewritePath("/maxxbank/loans/(?<segment>.*)","/${segment}")
-									.addResponseHeader("X-Response-Time",LocalDateTime.now().toString()))
+									.addResponseHeader("X-Response-Time",LocalDateTime.now().toString())
+									.retry(retryConfig -> retryConfig.setRetries(3)
+											.setMethods(HttpMethod.GET)
+											.setBackoff(Duration.ofMillis(100),Duration.ofMillis(100),2,true)))
 							.uri("lb://LOANS")) // It should be in Upper Case because it will match with Eureka service Registery
 					.route(p -> p
 							.path("/maxxbank/cards/**")
