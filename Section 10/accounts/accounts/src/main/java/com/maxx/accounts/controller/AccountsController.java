2599f9bf960295e34f16811b67e95f64c807319a
mahendra.chawla <mahendra.chawla26@gmail.com>
2024-09-18 17:00:00 +0530

    Implementing retry pattern in account service with gateway circuit breaker and Customize default timeout.

--- Section 10/accounts/accounts/src/main/java/com/maxx/accounts/controller/AccountsController.java
+++ Section 10/accounts/accounts/src/main/java/com/maxx/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.maxx.accounts.dto.CustomerDto;
 import com.maxx.accounts.dto.ErrorResponseDto;
 import com.maxx.accounts.dto.ResponseDto;
 import com.maxx.accounts.service.IAccountsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,6 +16,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -32,6 +35,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
 public class AccountsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
 
     private final IAccountsService iAccountsService;
 
@@ -199,11 +204,21 @@ public class AccountsController {
                     )
             }
     )
+    @Retry(name = "getBuildInfo", fallbackMethod = "getBuildInfoFallback")
     @GetMapping("/build-info")
     public ResponseEntity<String> getBuildInfo(){
+        logger.debug("getBuildInfo()methodInvoked");
+        throw new NullPointerException();
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .body(buildVersion);
+    }
+
+    public ResponseEntity<String> getBuildInfoFallback(Throwable throwable){
+        logger.debug("getBuildInfoFallback()methodInvoked");
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(buildVersion);
+                .body("0.9");
     }
 
     @Operation(
